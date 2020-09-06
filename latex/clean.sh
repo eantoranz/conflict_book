dbf78231c0fd24fb10dc18ad2d49a31874032dd1
Edmundo Carmona Antoranz <eantoranz@gmail.com>
2020-09-06 14:34:39 -0600

    scripts: clean script was not deleting html directory

--- latex/clean.sh
+++ latex/clean.sh
@@ -3,7 +3,7 @@
 # remove all intermediate/output files
 rm *.pdf *.html *.log *.lg *.idv *.out *.4ct *.4tc *.dvi *.aux *.css  *.tmp *.xref
 
-if [ -f html ]; then
+if [ -d html ]; then
 	rm html -fR
 fi
 
